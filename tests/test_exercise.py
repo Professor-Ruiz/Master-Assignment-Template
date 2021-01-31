3d27f5d7ccc93e0ef0e67ddf0450245bad85d037
Bianca Ruiz <69026509+RuizTheRuler@users.noreply.github.com>
2021-01-31 10:45:32 -0700

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -2,29 +2,41 @@ import pytest
 import src.exercise
 
 def test_case_1(capsys):
-    input_values = ["Snickerdoodle Cupcake","Biscoff Lava","Pumpkin Chocolate Chip"]
+    ''' Captures the output of the program, and verifies it alphabetizes the input '''
     
+    # Test input and output:
+    input_values = ["Snickerdoodle Cupcake","Biscoff Lava","Pumpkin Chocolate Chip"]
+    assert_out =   '\nBiscoff Lava\nPumpkin Chocolate Chip\nSnickerdoodle Cupcake\n'
+    
+    # Cycle the sample input through the student program:
     def mock_input(s):
         return input_values.pop(0)
     src.exercise.input = mock_input
     
+    # Execute the student program, and capture the output (print stateements):
     src.exercise.main()
-    
     out, err = capsys.readouterr()
     
-    assert out == '\nBiscoff Lava\nPumpkin Chocolate Chip\nSnickerdoodle Cupcake\n'
+    # Test the actual program output against the anticipated program output:
+    assert out == assert_out
     assert err == ''
 
 def test_case_2(capsys):
-    input_values = ["Lemon Glaze","Confetti Cake","Reese's Peanut Butter Chip"]
+    ''' Captures the output of the program, and verifies it alphabetizes the input '''
     
+    # Test input and output:
+    input_values = ["Lemon Glaze","Confetti Cake","Reese's Peanut Butter Chip"]
+    assert_out =   "\nConfetti Cake\nLemon Glaze\nReese's Peanut Butter Chip\n"    
+    
+    # Cycle the sample input through the student program:
     def mock_input(s):
         return input_values.pop(0)
     src.exercise.input = mock_input
     
+    # Execute the student program, and capture the output (print stateements):
     src.exercise.main()
-    
     out, err = capsys.readouterr()
     
-    assert out == "\nConfetti Cake\nLemon Glaze\nReese's Peanut Butter Chip\n"
-    assert err == ''   
+    # Test the actual program output against the anticipated program output:
+    assert out == assert_out
+    assert err == ''  
