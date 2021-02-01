13570b0a9b491504404d5c71fb5d90786b1ce9b0
Bianca Ruiz <69026509+RuizTheRuler@users.noreply.github.com>
2021-02-01 00:26:58 -0700

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,33 @@
-def main():
-  # Enter your code below:
-  
+def main(): 
+    name1 = input("Enter flavor one: ")
+    name2 = input("Enter flavor two: ")
+    name3 = input("Enter flavor three: ")
+    print()
+
+    if name2 > name1 < name3:
+        print(name1)
+        if name2 < name3:
+            print(name2)
+            print(name3)
+        else:
+            print(name3)
+            print(name2)
+    elif name1 > name2 < name3:
+        print(name2)
+        if name1 < name3:
+            print(name1)
+            print(name3)
+        else:
+            print(name3)
+            print(name1)
+    else:
+        print(name3)
+        if name1 < name2:
+            print(name1)
+            print(name2)
+        else:
+            print(name2)
+            print(name1)
+    
 if __name__ == '__main__':
-  main()
+    main()
