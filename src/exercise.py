1bc898072170df5249b41ff4a8581c8c62e888c8
Bianca Ruiz <69026509+RuizTheRuler@users.noreply.github.com>
2021-02-05 11:48:07 -0700

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,7 +1,7 @@
 def main():
     # delete the 'pass' statement, then begin coding below:
-    name = input("What's your name? ")
-    print(name)
+    pass
+
 
 if __name__ == '__main__': 
     main()
