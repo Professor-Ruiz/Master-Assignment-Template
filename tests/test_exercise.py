fa7888205e09c8da34cac578dc171246fdaeef2a
Bianca Ruiz <69026509+RuizTheRuler@users.noreply.github.com>
2021-02-05 11:35:50 -0700

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -4,11 +4,13 @@ import src.exercise
 inp_1 = ['bianca']
 out_1 = ['bianca']
 
-def test_capture_stdout(capsys, inp_1, out_1):
+# run the test function for each input/output pair
+@pytest.mark.parametrize("test_input, expected", [(inp_1, out_1)])
+def test_capture_stdout(capsys, test_input, expected):
     
     # Load the test input for the program execution:
     def mock_input(s):
-        return inp_1.pop(0)
+        return test_input.pop(0)
     src.exercise.input = mock_input
     
     # Execute the student program, and capture the output (print statements):
@@ -21,4 +23,4 @@ def test_capture_stdout(capsys, inp_1, out_1):
     out = [i for i in out if i]
 
     # Test the actual program output against the anticipated program output:
-    assert out == out_1
+    assert out == expected
