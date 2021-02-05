5c911832ed047cc9d50ed73927e327c704f49b87
Bianca Ruiz <69026509+RuizTheRuler@users.noreply.github.com>
2021-02-04 19:43:41 -0700

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,7 +1,9 @@
 '''
 TO DO:
 
-1. Open test-options.md (../docs/test-options.md).
-2. Choose a test to copy and paste into this file.
-3. Commit and push.
+1. Delete 'assert True' below 
+2. Open test-options.md (../docs/test-options.md).
+3. Choose a test to copy and paste into this file.
+4. Commit and push.
 '''
+assert True
