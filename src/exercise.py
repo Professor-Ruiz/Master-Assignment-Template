1ec5f0e07805e6d08b3c0150f5dec3dd3783a26e
Bianca Ruiz <69026509+RuizTheRuler@users.noreply.github.com>
2021-02-04 20:04:06 -0700

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -2,6 +2,5 @@ def main():
     # delete the 'pass' statement, then begin coding below:
     pass
 
-if __name__ == '__main__':
-    # don't worry about this code.    
+if __name__ == '__main__': 
     main()
