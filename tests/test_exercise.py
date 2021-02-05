8c647d3be6134a130dd158e9e4191d63a5ccc8bf
Bianca Ruiz <69026509+RuizTheRuler@users.noreply.github.com>
2021-02-04 19:49:38 -0700

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,11 +1,13 @@
 '''
 TO DO:
 
-1. Delete the three lines of code below 
+1. Delete the filler code below 
 2. Open test-options.md (../docs/test-options.md).
 3. Choose a test to copy and paste into this file.
 4. Commit and push.
 '''
 import pytest
+import src.exercise
+
 def dummy_test():
   assert True
