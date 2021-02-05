c04fe4a79f0f659d65325729c339052fb073cb07
Bianca Ruiz <69026509+RuizTheRuler@users.noreply.github.com>
2021-02-05 11:51:17 -0700

    Update test options (#7)
    Removed superfluous code in docs/testing-options.md parametrizing single input/output and only-output testing functions

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,10 +1,3 @@
-'''
-TO DO:
-
-1. Open test-options.md (../docs/test-options.md).
-2. Choose a test to copy and paste into this file.
-3. Commit and push.
-'''
 import pytest
 import src.exercise
 
